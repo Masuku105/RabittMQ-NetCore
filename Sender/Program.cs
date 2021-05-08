585224632e3085d30603047f6558a445479b0519
Xolani <masuku105@gmail.com>
2021-05-08 04:31:05 +0200

    First Commit

---
+++ Sender/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sender.Interfaces;
+using System;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = StartUp.ConfigurServices();
+            var sender = container.GetRequiredService<IMessageHandler>();
+            sender.Send();
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
