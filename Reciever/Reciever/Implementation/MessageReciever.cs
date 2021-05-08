643baf79b706ea094280fc429aca869ecb3e2a92
Masuku105 <masuku105@gmail.com>
2021-05-08 04:58:19 +0200

    Receiver

---
+++ Reciever/Reciever/Implementation/MessageReciever.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Reciever.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reciever.Implementation
+{
+    public class MessageReciever : IMessageHandler
+    {
+        private readonly IConnect _connect;
+
+        public MessageReciever(IConnect connect)
+        {
+            _connect = connect;
+        }
+        public void RecieveMessage()
+        {
+            using (var connection = _connect.GetConnection().CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                Console.WriteLine("Waiting for messages....");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("", message);
+                };
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                
+            }
+        }
+    }
+}
